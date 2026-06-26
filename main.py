41bad8ed6ad587bc2f6ccf2c773f79eea423cd0d
muhammadalikadirov39-droid <muhammadalikadirov39@gmail.com>
2026-06-26 23:34:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,72 @@
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    ContextTypes,
+    filters,
+)
 
+TOKEN =8763491267:AAEjYPugoZkw6N2BGxWHNx3yumgt5iVONak
+
+CHANNEL_ID = "@UstaTop_Kokand"
+
+NAME, JOB, EXPERIENCE, PHONE = range(4)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("👋 Assalomu alaykum!\n\nIsmingizni kiriting:")
+    return NAME
+
+async def get_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["name"] = update.message.text
+    await update.message.reply_text("🔨 Kasbingizni kiriting:")
+    return JOB
+
+async def get_job(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["job"] = update.message.text
+    await update.message.reply_text("📅 Ish tajribangizni kiriting:")
+    return EXPERIENCE
+
+async def get_experience(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["experience"] = update.message.text
+    await update.message.reply_text("📞 Telefon raqamingizni kiriting:")
+    return PHONE
+async def get_phone(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["phone"] = update.message.text
+
+    text = f"""
+🆕 Yangi usta
+
+👤 Ism: {context.user_data['name']}
+🔨 Kasb: {context.user_data['job']}
+📅 Ish tajribasi: {context.user_data['experience']}
+📞 Telefon: {context.user_data['phone']}
+"""
+
+    await context.bot.send_message(chat_id=CHANNEL_ID, text=text)
+    await update.message.reply_text("✅ Rahmat! Anketangiz kanalga yuborildi.")
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("❌ Bekor qilindi.")
+    return ConversationHandler.END
+
+
+app = Application.builder().token(TOKEN).build()
+
+conv = ConversationHandler(
+    entry_points=[CommandHandler("start", start)],
+    states={
+        NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_name)],
+        JOB: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_job)],
+        EXPERIENCE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_experience)],
+        PHONE: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_phone)],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
+
+app.add_handler(conv)
+
+app.run_polling()
